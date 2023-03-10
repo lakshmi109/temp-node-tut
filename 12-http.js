a5bb884e6e22b652e0a6c24f16668e51fe7096bf
lakshmi109 <muduli.1@iitj.ac.in>
2023-03-10 16:05:43 +0530

    first commit

---
+++ 12-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        res.write('Welcome to our home page')
+    }
+    else if(req.url === '/about'){
+        res.write('Here is our short history')
+    }
+    else {
+        res.write(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you're looking for</p>
+        <a href="/">Back Home</a>
+        `)
+    }
+    res.end()
+})
+
+server.listen(5000);
